ab1cb4eb46b503e3acd55d3cd227c03b36e63d88
Vynlar <lightningzygella@gmail.com>
2014-06-09 09:47:57 -0600

    Tried fixing the exhibit generation

--- platforms/android/assets/www/js/list-load.js
+++ platforms/android/assets/www/js/list-load.js
@@ -3,20 +3,22 @@ function generateExhibitList(data, id)
     $.each(data, function(i, _data) {
         var tag = $("<li></li>");
         var linkTag = $("<a></a>");
+        linkTag.attr("rel", "external");
         $("<img></img>").attr("src", _data[1]).appendTo(linkTag);
         $("<h2></h2>").html(_data[0]).appendTo(linkTag);
         $("<p></p>").html(_data[2]).appendTo(linkTag);
-        /*var popup = $("<div></div>");
-        popup.attr("data-role", "page");
+        var popup = $("<div></div>");
+        popup.attr("data-role", "dialog");
         popup.attr("data-add-back-button", "true");
-        linkTag.attr("data-transition", "slide");
+        linkTag.attr("data-transition", "pop");
         linkTag.attr("data-split-icon", "grid");
         var divId = _data[0].replace(/ /g, "-");
         popup.attr("id", divId);
-        $("<p></p>").html(_data[2]).appendTo(popup);*/
+        $("<p></p>").html(_data[2]).appendTo(popup);
         linkTag.attr("href", _data[3]);
         linkTag.appendTo(tag);
-        //tag.append(popup);
+        tag.append(popup);
         tag.appendTo(id);
     });
+    $(id).listview("refresh");
 }
