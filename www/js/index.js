0d6b0defe3066aae44d8f2c5e6a016adb3ece681
unknown <Nick.Degarmo@SL-ARTN53S-0397.site.bosqueschool.org>
2014-06-09 11:27:50 -0600

    put navbar stuff back inside pages, added data ids

--- www/js/index.js
+++ www/js/index.js
@@ -55,7 +55,7 @@ function loadHeader()
     });   
 }
 
-$(function() {
+/*$(function() {
     $("[data-role='navbar']" ).navbar();
     $("[data-role='header'], [data-role='footer']").toolbar();
 });
@@ -65,8 +65,10 @@ $( document ).on("pageshow", "[data-role='page']", function() {
     $("[data-role='navbar'] a.ui-btn-active").removeClass("ui-btn-active");
     $("[data-role='navbar'] a").each(function() {
         if ( $( this ).text() === current ) {
-            $( this ).addClass( "ui-btn-active" );
+            $( this ).addClass( "ui-btn-active ui-state-persist" );
         }
     }
     loadHeader();
-});
+});*/
+
+loadHeader();
